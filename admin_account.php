a2eddaf864bf705f397364bc7d34bfb20a935a8e
walidsalih111-ux <walidsalih111@gmail.com>
2026-04-28 08:52:13 +0800

    Initial commit

--- admin_account.php
+++ admin_account.php
@@ -31,7 +31,7 @@ $userProfile = $profileStmt->fetch(PDO::FETCH_ASSOC);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Account Settings | Admin</title>
+    <title>DICT Admin Account Settings</title>
     <link rel="icon" type="image/png" href="img/logo/DICT.png">
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
